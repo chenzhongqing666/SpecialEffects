bee956eb1d551c51edc57f6c223637163eb18490
陈忠庆 <66964903+chenzhongqing666@users.noreply.github.com>
2022-07-31 21:49:13 +0800

    feat:可定制化宽度

--- liuxing/index.js
+++ liuxing/index.js
@@ -1,15 +1,26 @@
+const pecent = 0.5
 
+document.write('<link rel="stylesheet" href="./index.css">');
+const body = document.getElementsByTagName('body')[0]
+const container = document.createElement('div')
+const fatherDom1 = document.createElement('div')
+const fatherDom2 = document.createElement('div')
+container.className='container'
+fatherDom1.className='father1'
+fatherDom2.className='father2'
+container.appendChild(fatherDom1)
+container.appendChild(fatherDom2)
+body.appendChild(container)
+container.style.width = '100vw'
+container.style.height = '100vh'
 
-const fatherDom1 = document.getElementsByClassName('father1')[0]
-const fatherDom2 = document.getElementsByClassName('father2')[0]
-
-const clientWidth = document.body.clientWidth
-const clientHeight = document.body.clientHeight
-const setLong = clientHeight>clientWidth?clientWidth:clientHeight
-fatherDom1.style.width = setLong*2+'px'
-fatherDom2.style.width = setLong*2+'px'
-fatherDom1.style.height = setLong+'px'
-fatherDom2.style.height = setLong+'px'
+const clientWidth = container.clientWidth
+const clientHeight = container.clientHeight
+const setLong = clientHeight > clientWidth ? clientWidth : clientHeight
+fatherDom1.style.width = setLong * 2 + 'px'
+fatherDom2.style.width = setLong * 2 + 'px'
+fatherDom1.style.height = setLong + 'px'
+fatherDom2.style.height = setLong + 'px'
 
 class Meteors {
     constructor(options) {
@@ -29,15 +40,15 @@ function createRandom(min = 0, max = 1) {
 
 function createDom() {
     const meteor1 = new Meteors({
-        width: createRandom(setLong*0.46, setLong*0.16),
-        height: createRandom(setLong*0.16, 10),
+        width: createRandom(setLong * 0.46*pecent, setLong * 0.16*pecent),
+        height: createRandom(setLong * 0.16*pecent, 10),
         top: createRandom(0, 100),
         background: `linear-gradient(to left,rgba(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()}),rgba(0,0,0,0))`
     })
     meteor1.innerDiv(fatherDom1)
     const meteor2 = new Meteors({
-        width: createRandom(setLong*0.46, setLong*0.16),
-        height: createRandom(setLong*0.16, 10),
+        width: createRandom(setLong * 0.46*pecent, setLong * 0.16*pecent),
+        height: createRandom(setLong * 0.16*pecent, 10),
         top: createRandom(0, 100),
         background: `linear-gradient(to left,rgba(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()}),rgba(0,0,0,0))`
     })
@@ -61,7 +72,7 @@ class Gauss {
 }
 
 function createGaussDom() {
-    const long1 = createRandom(setLong*0.05, setLong*0.02)
+    const long1 = createRandom(setLong * 0.05*pecent, setLong * 0.02*pecent)
     const gauss1 = new Gauss({
         width: long1,
         height: long1,
@@ -70,7 +81,7 @@ function createGaussDom() {
         background: `rgb(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()})`,
     })
     gauss1.innerDiv(fatherDom1)
-    const long2 = createRandom(setLong*0.05, setLong*0.02)
+    const long2 = createRandom(setLong * 0.05*pecent, setLong * 0.02*pecent)
     const gauss2 = new Gauss({
         width: long2,
         height: long2,
