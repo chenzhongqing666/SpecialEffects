68bd8c954a96be295d88373d3c76d7dadcef342c
陈忠庆 <66964903+chenzhongqing666@users.noreply.github.com>
2022-07-31 21:19:57 +0800

    init

---
+++ liuxing/index.js
@@ -0,0 +1,85 @@
+
+
+const fatherDom1 = document.getElementsByClassName('father1')[0]
+const fatherDom2 = document.getElementsByClassName('father2')[0]
+
+const clientWidth = document.body.clientWidth
+const clientHeight = document.body.clientHeight
+const setLong = clientHeight>clientWidth?clientWidth:clientHeight
+fatherDom1.style.width = setLong*2+'px'
+fatherDom2.style.width = setLong*2+'px'
+fatherDom1.style.height = setLong+'px'
+fatherDom2.style.height = setLong+'px'
+
+class Meteors {
+    constructor(options) {
+        this.options = options
+    }
+    innerDiv(dom) {
+        const createDom = document.createElement('div')
+        createDom.className = 'meteor'
+        createDom.style.cssText = `width:${this.options.width}px;height:${this.options.height}px;background:${this.options.background};top:calc(${this.options.top}% - ${this.options.height}px);border-radius:500px`
+        dom.appendChild(createDom)
+        setTimeout(() => { dom.removeChild(createDom) }, 20000)
+    }
+}
+function createRandom(min = 0, max = 1) {
+    return random = (Math.random() * (max - min) + min).toFixed(2)
+}
+
+function createDom() {
+    const meteor1 = new Meteors({
+        width: createRandom(setLong*0.46, setLong*0.16),
+        height: createRandom(setLong*0.16, 10),
+        top: createRandom(0, 100),
+        background: `linear-gradient(to left,rgba(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()}),rgba(0,0,0,0))`
+    })
+    meteor1.innerDiv(fatherDom1)
+    const meteor2 = new Meteors({
+        width: createRandom(setLong*0.46, setLong*0.16),
+        height: createRandom(setLong*0.16, 10),
+        top: createRandom(0, 100),
+        background: `linear-gradient(to left,rgba(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()}),rgba(0,0,0,0))`
+    })
+    meteor2.innerDiv(fatherDom2)
+    return createDom
+}
+
+setInterval(createDom(), 1000)
+
+class Gauss {
+    constructor(options) {
+        this.options = options
+    }
+    innerDiv(dom) {
+        const createDom = document.createElement('div')
+        createDom.className = 'gauss'
+        createDom.style.cssText = `width:${this.options.width}px;height:${this.options.height}px;background:${this.options.background};top:${this.options.top}vh;left:${this.options.left}vw;border-radius:50%`
+        dom.appendChild(createDom)
+        setTimeout(() => { dom.removeChild(createDom) }, 10000)
+    }
+}
+
+function createGaussDom() {
+    const long1 = createRandom(setLong*0.05, setLong*0.02)
+    const gauss1 = new Gauss({
+        width: long1,
+        height: long1,
+        top: createRandom(0, 100),
+        left: createRandom(0, 50),
+        background: `rgb(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()})`,
+    })
+    gauss1.innerDiv(fatherDom1)
+    const long2 = createRandom(setLong*0.05, setLong*0.02)
+    const gauss2 = new Gauss({
+        width: long2,
+        height: long2,
+        top: createRandom(0, 100),
+        left: createRandom(0, 50),
+        background: `rgb(${createRandom(0, 256)},${createRandom(0, 256)},${createRandom(0, 256)},${createRandom()})`,
+    })
+    gauss2.innerDiv(fatherDom2)
+    return createGaussDom
+}
+
+setInterval(createGaussDom(), 1000)
